97b94125697dbd2198899d8cf2464c326b00e774
martinberlin <martin@luckycloud.de>
2019-05-28 19:16:26 +0200

    Add setupConfig where you can edit your WiFI Credentials

--- Micophone-Mesh/Micophone-Mesh.ino
+++ Micophone-Mesh/Micophone-Mesh.ino
@@ -1,15 +1,9 @@
-#include <WiFi.h>
 #include <HTTPClient.h>
 #include <M5StickC.h>
-#include <driver/i2s.h>
-//#include <Connect.cpp>  
-// UPDATE with your connection
-#define WIFISSID "KabelBox-A210"
-#define WIFIPASS ""
 
 #define PIN_CLK  0
 #define PIN_DATA 34
-#define READ_LEN (2 * 1024)
+#define READ_LEN (1 * 1024)
 uint8_t BUFFER[READ_LEN] = {0};
 
 uint16_t oldx[160];
@@ -17,44 +11,19 @@ uint16_t oldy[160];
 uint16_t *adcBuffer = NULL;
 HTTPClient http;  
 
-void i2sInit()
-{
-   i2s_config_t i2s_config = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM),
-    .sample_rate =  44100,
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT, // is fixed at 12bit, stereo, MSB
-    .channel_format = I2S_CHANNEL_FMT_ALL_RIGHT,
-    .communication_format = I2S_COMM_FORMAT_I2S,
-    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = 2,
-    .dma_buf_len = 128,
-   };
-
-   i2s_pin_config_t pin_config;
-   pin_config.bck_io_num   = I2S_PIN_NO_CHANGE;
-   pin_config.ws_io_num    = PIN_CLK;
-   pin_config.data_out_num = I2S_PIN_NO_CHANGE;
-   pin_config.data_in_num  = PIN_DATA;
-	
-   
-   i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-   i2s_set_pin(I2S_NUM_0, &pin_config);
-   i2s_set_clk(I2S_NUM_0, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
-}
-
-
+#include "setupConfig.h"
 
 void mic_record_task() //void* arg
 {   
 
     i2s_read_bytes(I2S_NUM_0, (char*) BUFFER, READ_LEN, (100 / portTICK_RATE_MS));
     adcBuffer = (uint16_t *)BUFFER;
-    showSignal();
+    
     int signalLevel = 0;   
     for ( int i = 0; i < READ_LEN; i++ ) {
        signalLevel += adcBuffer[i];
     }
-    signalLevel = (signalLevel/READ_LEN)/120;
+    signalLevel = ((signalLevel/READ_LEN)/100)-200;
    Serial.print(signalLevel);Serial.println();
  
    http.begin("http://192.168.0.75/device_request");    // Specify destination for HTTP request
@@ -65,54 +34,21 @@ void mic_record_task() //void* arg
    
    String json = "{\"request\": \"set_status\",\"characteristics\": [{\"cid\": 1,\"value\": "+String(signalLevel)+"}]}";
    int httpResponseCode = http.POST(json);
-     if(httpResponseCode>0){
- 
+    
+    if(httpResponseCode>0){
     String response = http.getString();                       //Get the response to the request
- 
-    Serial.println(httpResponseCode);   //Print return code
+    //Serial.println(httpResponseCode);   //Print return code
     //Serial.println(response);           //Print request answer
  
    }else{
     Serial.print("Error on sending POST: ");
     Serial.println(httpResponseCode);
    }
- 
-   http.end(); 
    http.end();
+   // Let's focus first on sending the Light signal fast
+   //showSignal(); 
    //vTaskDelay(100 / portTICK_RATE_MS);
 }
-
-void setup() {
-  Serial.begin(115200);
-  M5.begin();
-  
- // TODO: Get IP via mDNS from : esp32_mesh.local
-
- 
-  M5.Lcd.println("Connect WIFI:");
-  M5.Lcd.println(WIFISSID);
-
-  WiFi.begin(WIFISSID, WIFIPASS); 
-  int count = 0;
-  while (WiFi.status() != WL_CONNECTED) { 
-    M5.Lcd.print(".");delay(100);
-    if (count>100) ESP.restart();
-    count++;
-  }
- 
-  M5.Lcd.println("Connected to WiFi");delay(500);
-  
-  M5.Axp.ScreenBreath(8); // Brightness (min and visible 7 - 10 max)
-  M5.Lcd.setRotation(3);
-  M5.Lcd.fillScreen(WHITE);
-  M5.Lcd.setTextColor(BLACK, WHITE);
-  M5.Lcd.println("MIC");
-
-  i2sInit();
-  // We will do this in the loop :)
-  //xTaskCreatePinnedToCore(mic_record_task, "mic_record_task", 2048, NULL, 1, NULL, 1);
-}
-
 
 void showSignal(){
   int x, y;
@@ -128,5 +64,5 @@ void showSignal(){
 
 void loop() {
   mic_record_task();
-  delay(100);
+  delay(10);
 }
---
+++ Micophone-Mesh/setupConfig.h
@@ -0,0 +1,64 @@
+// UPDATE with your connection
+#define WIFISSID "KabelBox-A210"
+#define WIFIPASS ""
+
+#include <WiFi.h>
+#include <driver/i2s.h>
+
+void i2sInit()
+{
+   i2s_config_t i2s_config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM),
+    .sample_rate =  44100,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT, // is fixed at 12bit, stereo, MSB
+    .channel_format = I2S_CHANNEL_FMT_ALL_RIGHT,
+    .communication_format = I2S_COMM_FORMAT_I2S,
+    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+    .dma_buf_count = 2,
+    .dma_buf_len = 128,
+   };
+
+   i2s_pin_config_t pin_config;
+   pin_config.bck_io_num   = I2S_PIN_NO_CHANGE;
+   pin_config.ws_io_num    = PIN_CLK;
+   pin_config.data_out_num = I2S_PIN_NO_CHANGE;
+   pin_config.data_in_num  = PIN_DATA;
+  
+   
+   i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+   i2s_set_pin(I2S_NUM_0, &pin_config);
+   i2s_set_clk(I2S_NUM_0, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  M5.begin();
+  
+ // TODO: Get IP via mDNS from : esp32_mesh.local
+
+ 
+  M5.Lcd.println("Connect WIFI:");
+  M5.Lcd.println(WIFISSID);
+
+  WiFi.begin(WIFISSID, WIFIPASS); 
+  int count = 0;
+  while (WiFi.status() != WL_CONNECTED) { 
+    M5.Lcd.print(".");delay(100);
+    if (count>100) ESP.restart();
+    count++;
+  }
+ 
+  M5.Lcd.println("Connected to WiFi");delay(500);
+  
+  M5.Axp.ScreenBreath(8); // Brightness (min and visible 7 - 10 max)
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(WHITE);
+  M5.Lcd.setTextColor(BLACK, WHITE);
+  M5.Lcd.println("MIC");
+
+  i2sInit();
+  // We will do this in the loop :)
+  //xTaskCreatePinnedToCore(mic_record_task, "mic_record_task", 2048, NULL, 1, NULL, 1);
+}
+
